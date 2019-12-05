20e45e2cef02d7b412030ec3318091d207a989b2
Arina Zaitseva <avanero@bk.ru>
2019-12-05 17:10:49 +0700

    lesson2

---
+++ purchase.rb
@@ -0,0 +1,10 @@
+while 
+puts "Enter the name of the product: "
+name = gets.chomp
+puts "Enter the price: "
+price = gets.chomp
+puts "Enter the quantity: "
+quant = gets.chomp
+break if name == "stop"
+end
+
