ad68112e1f97ae70d55d17f3fd2a94e0d5522ecf
Arina Zaitseva <avanero@bk.ru>
2019-12-05 21:17:07 +0700

    vowels corrected

--- vowels.rb
+++ vowels.rb
@@ -1,11 +1,11 @@
 
 
-vowels = [a,e,i,o,u,y]
+vowels = ["a","e","i","o","u","y"]
 
 hash = Hash.new
 
 i = 0
-vowels.each do |v| hash[v] = (i++)
+vowels.each do |v| hash[v] = (i + 1)
 	i += 1
 end
 
